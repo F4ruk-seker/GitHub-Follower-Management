c830619a81cd0f30ca7636410112fd48bfdafc58
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-10-27 15:58:34 +0300

    Change typing

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from reporter import write_html_report
 from models import GitHubUser
 from webbrowser import open_new_tab
-import requests
+from requests import api
 import config
 
 
-def fetch_github_users(url):
+def fetch_github_users(url: str) -> list[GitHubUser] | None:
     headers = {
         "Authorization": f"token {config.access_token}"
     }
-    response = requests.get(url, headers=headers)
+    response = api.get(url, headers=headers)
     if response.status_code == 200:
         return [GitHubUser(**user_data) for user_data in response.json()]
     else:
         raise f"Error fetching data from {url}: {response.status_code}"
 
 
-def compare_followers_and_following(username):
-    followers_url = f"https://api.github.com/users/{username}/followers"
-    following_url = f"https://api.github.com/users/{username}/following"
+def compare_followers_and_following(username: str) -> dict[str, list]:
+    followers_url: str = f"https://api.github.com/users/{username}/followers"
+    following_url: str = f"https://api.github.com/users/{username}/following"
 
-    followers = fetch_github_users(followers_url)
-    following = fetch_github_users(following_url)
+    followers: list[GitHubUser] | None = fetch_github_users(followers_url)
+    following: list[GitHubUser] | None = fetch_github_users(following_url)
 
-    follower_logins = {follower.login for follower in followers}
-    following_logins = {followed.login for followed in following}
+    follower_logins: set = {follower.login for follower in followers}
+    following_logins: set = {followed.login for followed in following}
 
-    class Report:
-        not_following_back = [user for user in following if user.login not in follower_logins]
-        not_followed_back = [user for user in followers if user.login not in following_logins]
-
-    return Report
+    return {
+        'not_following_back': [user for user in following if user.login not in follower_logins],
+        'not_followed_back': [user for user in followers if user.login not in following_logins]
+    }
 
 
 def main():
-    report = compare_followers_and_following(config.username)
-    report_output_dir = write_html_report(**report.__dict__)
-    open_new_tab(report_output_dir)
+    report: dict = compare_followers_and_following(config.username)
+    report_output_dir: str = write_html_report(report)
+
+    open_new_tab(report_output_dir)  # -> None
 
 
 if __name__ == '__main__':
--- reporter.py
+++ reporter.py
@@ -3,13 +3,13 @@ import os
 import tempfile
 
 
-def write_html_report(**context) -> str:
+def write_html_report(context: dict) -> str | None:
     new_temp_dir = tempfile.mkdtemp()
     template_file_name = 'report_template.html'
     with open(template_file_name, encoding='utf-8') as template_file:
         template = Template(template_file.read())
-        output = template.render(context)
-        output_path = os.path.join(new_temp_dir, 'github-report-f4.html')
-        with open(output_path, 'w', encoding='utf-8') as output_file:
-            output_file.write(output)
-        return output_path
+    output = template.render(context)
+    output_path = os.path.join(new_temp_dir, 'github-report-f4.html')
+    with open(output_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(output)
+    return output_path
